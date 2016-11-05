a7ac96802217a737702d4d0f0f8639dbadff6db5
zh <zh2015211203@bupt.edu.cn>
2016-11-05 16:06:49 +0800

    改了包名

---
+++ TestHuan/app/src/main/java/com/tongxin/youni/bean/ExpressDao.java
@@ -0,0 +1,35 @@
+package com.tongxin.youni.bean;
+
+/**
+ * Created by yangxiang on 2016/9/28.
+ */
+
+public class ExpressDao {
+    public static final String InstallationId = "express_installationID";
+    public static final String username = "express_username";
+    public static final String company = "express_company";
+    public static final String phone = "express_phone";
+    public static final String roomId = "express_roomId";
+    public static final String size = "express_size";
+    public static final String state = "express_state";
+    public static final String isFragile = "express_fragile";
+    public static final String extra = "express_extra";
+    public static final String userID = "express_userId";
+    public static final String time = "express_time";
+    public static final String type = "express_type";
+    public static final String money = "express_money";
+
+    public static final int isWaiting = 1;
+    public static final int isTaking = 2;
+    public static final int isFinished = 3;
+
+    public static final int big = 4;
+    public static final int midden = 5;
+    public static final int small = 6;
+
+//    public static final int clothes = 1;
+//    public static final int dailyUse = 2;
+//    public static final int electronic = 3;
+//    public static final int food = 4;
+//    public static final int others = 5;
+}
